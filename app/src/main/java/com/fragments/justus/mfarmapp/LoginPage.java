30d32443224eebc877effaab4b32947fba10bf85
justus <jmbaluka@mfarm.co.ke>
2015-07-01 16:53:09 +0300

    finished working on login prototype

--- app/src/main/java/com/fragments/justus/mfarmapp/LoginPage.java
+++ app/src/main/java/com/fragments/justus/mfarmapp/LoginPage.java
@@ -1,18 +1,31 @@
 package com.fragments.justus.mfarmapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginPage extends Activity {
-
+  private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        btnLogin =(Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(),NavigationDrawer.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
