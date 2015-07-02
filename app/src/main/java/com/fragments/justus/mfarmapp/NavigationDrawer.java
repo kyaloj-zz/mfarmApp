bfe4b0d21a0fd8bfa8f1370ceb1ff19ce6884168
justus <jmbaluka@mfarm.co.ke>
2015-07-02 08:28:45 +0300

    added listview in navDrawer class..for now

--- app/src/main/java/com/fragments/justus/mfarmapp/NavigationDrawer.java
+++ app/src/main/java/com/fragments/justus/mfarmapp/NavigationDrawer.java
@@ -6,17 +6,42 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 import it.neokree.materialnavigationdrawer.elements.MaterialSection;
 
 
-public class NavigationDrawer extends MaterialNavigationDrawer {
+public class NavigationDrawer extends Activity {
 
     @Override
-    public void init(Bundle savedInstanceState) {
-        Intent intent = new Intent(getBaseContext(),Trends.class);
-        MaterialSection section = newSection("Section 1", intent);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+        String[] list = {"Profile", "Daily prices", "Trends", "Blog", "Planting guide","Marketplace", "About MFarm", "Setttings", "Logout"};
+        ListView myList = (ListView)findViewById(R.id.listView);
+
+        ListAdapter listAdapter = new ArrayAdapter<String>(this,R.layout.mylistviewitems,R.id.textView,list);
+        myList.setAdapter(listAdapter);
+
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String toast = String.valueOf(adapterView.getItemAtPosition(i));
+                Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT).show();
+                switch (i) {
+                    case 0:
+
+
+                }
+
+            }
+        });
 
     }
 }
