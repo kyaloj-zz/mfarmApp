675e26a9e5aa4d1808ae63a82b950a2e121e6967
justus <jmbaluka@mfarm.co.ke>
2015-07-02 14:00:43 +0300

    some more stuff

--- app/src/main/java/com/fragments/justus/mfarmapp/NavigationDrawer.java
+++ app/src/main/java/com/fragments/justus/mfarmapp/NavigationDrawer.java
@@ -2,6 +2,7 @@ package com.fragments.justus.mfarmapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,37 +14,34 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.fragments.justus.mfarmapp.mockedActivity.Settings;
+import com.fragments.justus.mfarmapp.mockedFragments.FragmentIndex;
+
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
+import it.neokree.materialnavigationdrawer.elements.MaterialAccount;
 import it.neokree.materialnavigationdrawer.elements.MaterialSection;
 
 
-public class NavigationDrawer extends Activity {
+public class NavigationDrawer extends MaterialNavigationDrawer {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation_drawer);
-        String[] list = {"Profile", "Daily prices", "Trends", "Blog", "Planting guide","Marketplace", "About MFarm", "Settings", "Logout"};
-        ListView myList = (ListView)findViewById(R.id.listView);
+    public void init(Bundle savedInstanceState) {
+        // add accounts
+        MaterialAccount account = new MaterialAccount(this.getResources(),"NeoKree","neokree@gmail.com", R.drawable.photo, R.drawable.bamboo);
+        this.addAccount(account);
 
-        ListAdapter listAdapter = new ArrayAdapter<String>(this,R.layout.mylistviewitems,R.id.textView,list);
-        myList.setAdapter(listAdapter);
+        MaterialAccount account2 = new MaterialAccount(this.getResources(),"Hatsune Miky","hatsune.miku@example.com",R.drawable.photo2,R.drawable.mat2);
+        this.addAccount(account2);
 
-        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String toast = String.valueOf(adapterView.getItemAtPosition(i));
-                Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getBaseContext(),Trends.class);
-                startActivity(intent);
-                switch (i) {
-                    case 0:
+        MaterialAccount account3 = new MaterialAccount(this.getResources(),"Example","example@example.com",R.drawable.photo,R.drawable.mat3);
+        this.addAccount(account3);
+
+        // create sections
+        this.addSection(newSection("Section 1", R.drawable.ic_mic_white_24dp, new FragmentIndex()));
+        this.addSection(newSection("Section 2", R.drawable.ic_hotel_grey600_24dp, new FragmentIndex()));
 
 
-                }
-
-            }
-        });
-
+        // create bottom section
+        this.addBottomSection(newSection("Bottom Section", R.drawable.ic_settings_black_24dp, new Intent(this, Settings.class)));
     }
 }
---
+++ app/src/main/java/com/fragments/justus/mfarmapp/mockedActivity/Profile.java
@@ -0,0 +1,18 @@
+package com.fragments.justus.mfarmapp.mockedActivity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.fragments.justus.mfarmapp.R;
+
+/**
+ * Created by justus on 7/2/15.
+ */
+public class Profile extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+
+    }
+}
