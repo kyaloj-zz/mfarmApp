c95c18db95a176f3c49c2b88c241c1ccb7cb90fb
justus <jmbaluka@mfarm.co.ke>
2015-07-02 10:04:06 +0300

    added trends class

--- app/src/main/java/com/fragments/justus/mfarmapp/NavigationDrawer.java
+++ app/src/main/java/com/fragments/justus/mfarmapp/NavigationDrawer.java
@@ -23,7 +23,7 @@ public class NavigationDrawer extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_drawer);
-        String[] list = {"Profile", "Daily prices", "Trends", "Blog", "Planting guide","Marketplace", "About MFarm", "Setttings", "Logout"};
+        String[] list = {"Profile", "Daily prices", "Trends", "Blog", "Planting guide","Marketplace", "About MFarm", "Settings", "Logout"};
         ListView myList = (ListView)findViewById(R.id.listView);
 
         ListAdapter listAdapter = new ArrayAdapter<String>(this,R.layout.mylistviewitems,R.id.textView,list);
@@ -34,6 +34,8 @@ public class NavigationDrawer extends Activity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String toast = String.valueOf(adapterView.getItemAtPosition(i));
                 Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getBaseContext(),Trends.class);
+                startActivity(intent);
                 switch (i) {
                     case 0:
 
